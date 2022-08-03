572c201b4568aaf2cfbcadf12faac1a1fd5f9fac
YouAreNotDefined <for.1self.ik@gmail.com>
2022-08-03 11:54:29 +0000

    cobra init

--- cmd/root.go
+++ cmd/root.go
@@ -1,62 +1,51 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
 package cmd
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
-
-	"github.com/spf13/viper"
 )
 
-var cfgFile string
 
-type CfgMap struct {
-	K string `toml:"K"`
-	V string `toml:"V"`
-}
 
-type Config struct {
-	Port      string   `toml:"Port"`
-	Path      []CfgMap `toml:"Path"`
-	IncludeId []CfgMap `toml:"IncludeId"`
-	Alternate []CfgMap `toml:"Alternate,omitempty"`
-}
-
-var config Config
-
+// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "localchecker",
-	Short: "Set up an http server",
-	Long:  `This application is a tool to set up an http server`,
-	Run: func(c *cobra.Command, args []string) {
-		fmt.Printf("configFile: %s\nconfig: %#v\n", cfgFile, config)
-	},
+	Use:   "collect-coursera-subtitles",
+	Short: "A brief description of your application",
+	Long: `A longer description that spans multiple lines and likely contains
+examples and usage of using your application. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
+	err := rootCmd.Execute()
+	if err != nil {
 		os.Exit(1)
 	}
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", "localchecker.toml", "config file")
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.collect-coursera-subtitles.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
-func initConfig() {
-	viper.SetConfigFile(cfgFile)
 
-	if err := viper.ReadInConfig(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	if err := viper.Unmarshal(&config); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-}
--- main.go
+++ main.go
@@ -1,3 +1,7 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
 import "github.com/YouAreNotDefined/collect-coursera-subtitles/cmd"
